14112d0d20091df367516f1532e9471994a36ec6
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-20 09:29:07 +0530

    Add files via upload

---
+++ Comments in CPP.cpp
@@ -0,0 +1,15 @@
+ 
+#include <iostream>
+using namespace std;
+//this program is written to understand comments in c++
+/*this
+is 
+multi 
+line 
+comment*/ 
+    int main(){
+        int sum=6;
+        cout<<"Hello world"<<sum;
+        return 0;
+    }
+
