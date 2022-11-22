ad0845a218b953b0f281de3b19c617eaa33fa21e
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-22 16:05:23 +0530

    Add files via upload

---
+++ addition in cpp.cpp
@@ -0,0 +1,21 @@
+// Addition in C++;
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int num1, num2;
+    cout<<"Enter the value of num1:\n"; // '<<' is called Insertion operator
+    cin>>num1; //'>>' is called Extraction operator
+
+    cout<<"Enter the value of num2:\n"; 
+    cin>>num2; 
+
+    cout<<"The sum of num1 & num2 is "<< num1+num2;
+
+    return 0;
+}
+
+
+
+
