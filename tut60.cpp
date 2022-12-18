b6ebe1eb43cbf710e6612938214d4377f30d9240
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-12-18 10:52:25 +0530

    Add files via upload

---
+++ tut60.cpp
@@ -0,0 +1,31 @@
+
+/* These are some useful classes for working with files in C++
+
+1.fstreambase
+2.ifstream --> derived from fstreambase
+3.ofstream --> derived from fstreambase
+
+In order to work with files in C++, you will have to open it. Primarily, there are 2 ways to open a file:
+
+1-Directly Using the constructor
+2-And Using the member function open() of the class  */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+  string st ="file me likhdo ,Harry bhai ";
+  // Opening files using constructor and writing it
+  ofstream out("sample60.txt"); //Write operation
+  out<<st;  
+
+  string st2;  
+ // Opening files using constructor and reading it
+  ifstream in("sample60b.txt");  //read operation 
+ // in>>st2;
+  getline(in,st2); 
+  cout<<st2;
+
+ return 0;
+}
